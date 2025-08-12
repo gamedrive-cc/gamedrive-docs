83f56f0567ac63bbcf25d81247ea9d7458ec2519
Nop Tanakhon <nop.tanakhon@gmail.com>
2025-08-12 10:20:46 +0700

    update docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.gamedrive.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kktech', // Usually your GitHub org/user name.
+  projectName: 'gamedrive', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -38,18 +38,19 @@ const config: Config = {
 
   presets: [
     [
-      'classic',
+      '@docusaurus/preset-classic',
       {
         docs: {
+          routeBasePath: '/', 
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
         },
+        blog: false, // Optional: disable blog
+        // pages: false, // Optional: disable static pages
       } satisfies Preset.Options,
     ],
   ],
@@ -57,7 +58,7 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     navbar: {
-      title: 'Home',
+      title: 'GameDrive',
       logo: {
         alt: 'GameDrive Logo',
         src: 'img/gamedrive-logo.png',
